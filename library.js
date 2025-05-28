03e2aa1075ea57144f5a79f49f51215fb458473f
meatwad787 <greg_mrjohnson@yahoo.com>
2025-05-28 11:26:36 -0400

    Add variable for the form-container change the z-index for the form-container when needed
    Add better comments

--- library.js
+++ library.js
@@ -1,4 +1,5 @@
 const Library = document.querySelector('#library');
+const FormConainer = document.querySelector('#form-container')
 const Form = document.querySelector('#form');
 const NewBookBtn = document.querySelector('#new-book');
 const Container = document.querySelector('#container');
@@ -8,17 +9,21 @@ const SubmitBtn = document.querySelector('#submit');
 
 // This makes the form "appear" as a popup 
 // and blurs the background when the form is on-screen
-//Later: remove the blur when the submit button is clicked
+//and switches the z-index of the form-container from -1 to 1;
 NewBookBtn.addEventListener('click', ()=> {
   Form.style.display = 'flex';
+  FormConainer.classList.add('z-index')
   Container.classList.add('blur')
 })
 
+//Prevents the form from trying to send data 
+//removes the blur from the page and 
+//resets the form-container's z-index to -1
 SubmitBtn.addEventListener('click', (event)=> {
   event.preventDefault();
   Container.classList.remove('blur')
   Form.style.display = 'none';
-
+  FormConainer.classList.remove('z-index')
   
 })
 
@@ -47,12 +52,15 @@ const booksToAdd = [
     { title: "To Kill a Mockingbird", author: "Harper Lee", pages: 281 }
   ];
   
-  // A Loop that looks through and adds each object to addBookToLibrary()
-  // Has to be here. Don't know why
+  //Loops through objects in booksToAdd and 
+  //runs them through addBookToLibrary...
+  //which then stores them in the 
+  // myLibrary  array (to be displayed later)
   for (let book of booksToAdd) {
     addBookToLibrary(book.title, book.author, book.pages);
   }
-  // !!! Figure out how to first display array to the page !!! 
+  // Clears the Library container and 
+  // creates a div to hold each book in booksToAdd
   Library.innerHTML = '';
   myLibrary.forEach(book => {
     // Create a div for each book
@@ -61,9 +69,9 @@ const booksToAdd = [
 
     // Fill the div with book info
     bookDiv.innerHTML = `
-      <p><strong>Title:</strong> ${book.title}</p>
-      <p><strong>Author:</strong> ${book.author}</p>
-      <p><strong>Pages:</strong> ${book.pages}</p>
+      <p>Title: ${book.title}</p>
+      <p>Author: ${book.author}</p>
+      <p>Pages: ${book.pages}</p>
     `+ '<br/>';
 
     // Add it to the page
